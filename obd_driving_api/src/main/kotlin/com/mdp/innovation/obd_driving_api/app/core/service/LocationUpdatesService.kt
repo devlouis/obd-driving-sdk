0505bb630500c1bfa923be757318fc4c539625fc
luisLopez <louislopez.dev@gmail.com>
2018-12-10 19:38:58 -0500

    Sprint 2 finish

--- obd_driving_api/src/main/kotlin/com/mdp/innovation/obd_driving_api/app/core/service/LocationUpdatesService.kt
+++ obd_driving_api/src/main/kotlin/com/mdp/innovation/obd_driving_api/app/core/service/LocationUpdatesService.kt
@@ -39,7 +39,7 @@ class LocationUpdatesService : Service() {
      * El intervalo deseado para las actualizaciones de ubicación Inexacto.
      * Las actualizaciones pueden ser más o menos frecuentes.
      */
-    private val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 1000
+    private val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 500
 
     /**
      * La tasa más rápida para actualizaciones de ubicación activa.
@@ -272,8 +272,8 @@ class LocationUpdatesService : Service() {
 
     private fun createLocationRequest() {
         mLocationRequest = LocationRequest()
-        mLocationRequest.interval = 1000
-        mLocationRequest.fastestInterval = 1000
+        mLocationRequest.interval = 500
+        mLocationRequest.fastestInterval = 500
         mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
     }
 
